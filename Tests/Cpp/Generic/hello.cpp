fcc723b11a023dd4189e7c86321ef0fb9fb9bb6f
Aditya Paluri <pv.aditya@gmail.com>
2017-12-10 12:47:54 +0530

    Initial Commit

---
+++ Tests/Cpp/Generic/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int ret = 0;
+	cout << "Hello World" << endl;
+	return ret;
+}
---
+++ Tests/Cpp/SystemC/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <systemc>
+
+using namespace std;
+
+int sc_main(int argc, char *argv[])
+{
+	int ret = 0;
+
+	cout << "Hello World" << endl;
+
+	return ret;
+}
